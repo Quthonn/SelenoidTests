7619012ac8ee85cf7673522d98c8a5a1fe835d04
quthonn <gevorgyan.genri@gmail.com>
2025-05-09 13:30:53 +0700

    Add check browser version

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
@@ -14,6 +15,7 @@ public class SelenoidTests {
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("total", is(5));
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("127.0"));
     }
 }
