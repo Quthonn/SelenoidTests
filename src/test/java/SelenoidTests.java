14b6175978287f98206f3ac95febfb6207bacc0e
quthonn <gevorgyan.genri@gmail.com>
2025-05-09 13:53:38 +0700

    added check wd hub status

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,3 +1,4 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
@@ -5,6 +6,8 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
+    private static String Login;
+    private static String Password;
     @Test
     void checkTotal() {
         given()
@@ -17,5 +20,22 @@ public class SelenoidTests {
                 .statusCode(200)
                 .body("total", is(5))
                 .body("browsers.chrome", hasKey("127.0"));
+    }
+
+    @Test
+    void checkWdHubStatus() {
+        Dotenv dotenv = Dotenv.load();
+        Login = dotenv.get("login");
+        Password = dotenv.get("password");
+        given()
+                .log().all()
+                .auth().basic(Login, Password)
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
     }
 }
