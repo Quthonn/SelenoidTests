ee984a00a0a43a64d4613a7beec9adb24999bca2
quthonn <gevorgyan.genri@gmail.com>
2025-05-09 13:25:34 +0700

    add logs

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,12 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
     @Test
     void checkTotal() {
-        get("https://selenoid.autotests.cloud/status")
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
                 .then()
+                .log().status()
+                .log().body()
                 .statusCode(200)
                 .body("total", is(5));
     }
